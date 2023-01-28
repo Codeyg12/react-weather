cac49b8058dea0c81973902e443a50414e038ad0
Codeyg12 <codey.gallup@gmail.com>
2023-01-27 22:20:48 -0700

    added card made in Figma for styling idea

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -1,28 +1,39 @@
 export default function CurrentWeather() {
-    let icon = '13d'
-    
+  let icon = "13d";
+
   return (
     <>
-      <div className="current-weather-card">
-        <div className="city-info">
-          <p className="name">Denver</p>
-          <p className="description">Cloudy</p>
-        </div>
-        <img src={`http://openweathermap.org/img/wn/${icon}@2x.png`} alt="" className="weather-icon" />
-        <div className="temp">{18}℉</div>
-        <div className="feels-like">
-          <span className="label">Feels Like</span>
-          <span className="detail">{2}℉</span>
-        </div>
-        <div className="wind">
-          <span className="label">Wind Speed</span>
-          <span className="detail">{45}MPH</span>
-        </div>
-        <div className="humid">
-          <span className="label">Humidity</span>
-          <span className="detail">{77}%</span>
+      <div className="current">
+        <div className="current-weather-card">
+          <div className="left">
+            <div className="city-info">
+              <p className="name">Denver</p>
+              <p className="description">Cloudy</p>
+            </div>
+            <div className="temp">{18}℉</div>
+          </div>
+          <div className="right">
+            <img
+              src={`http://openweathermap.org/img/wn/${icon}@2x.png`}
+              alt="weather-icon"
+              className="weather-icon"
+            />
+            <div className="feels-like">
+              <span className="label">Feels Like</span>
+              <span className="detail">{2}℉</span>
+            </div>
+            <div className="wind">
+              <span className="label">Wind Speed</span>
+              <span className="detail">{45}MPH</span>
+            </div>
+            <div className="humid">
+              <span className="label">Humidity</span>
+              <span className="detail">{77}%</span>
+            </div>
+          </div>
         </div>
       </div>
+      <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FMycwaEKLYe5fFJUjN3aEVF%2FUntitled%3Fnode-id%3D0%253A1%26t%3DbBjAVLDBZgMTyIPy-1" allowfullscreen></iframe>
     </>
   );
 }
