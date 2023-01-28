dbfa3f8eb67a3eb9322804f8afb4bd82969421fb
Codeyg12 <codey.gallup@gmail.com>
2023-01-27 20:22:55 -0700

    search bar added, city state saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
 import Searchbar from "./Searchbar";
 
 function App() {
+
+  const [city, setCity] = useState('')
+
   return (
     <>
-      <h1>Hello World</h1>
-      <Searchbar />
+      <h1 className="title">Weather Dashboard</h1>
+      <Searchbar setCity={setCity} />
     </>
   );
 }
--- src/Searchbar.jsx
+++ src/Searchbar.jsx
@@ -1,19 +1,23 @@
 import ReactGoogleAutocomplete from "react-google-autocomplete";
 import { GOOGLE_API_KEY } from "./api";
 
-export default function Searchbar() {
+export default function Searchbar({ setCity }) {
+
   return (
     <>
+    <div className="search-bar-div">
+      
       <ReactGoogleAutocomplete
         apiKey={GOOGLE_API_KEY}
         onPlaceSelected={(place) => {
-          console.log(place);
+          setCity(place.address_components[0].long_name);
         }}
-
-      />
+        className='search-bar'
+        />
+        </div>
     </>
   );
-}
+} 
 
 // import { GOOGLE_API_KEY } from "./api";
 // // import ReactGoogleAutocomplete from "react-google-autocomplete";
