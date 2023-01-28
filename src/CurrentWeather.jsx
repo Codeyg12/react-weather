d2a877373c8041ab951cd2b3a0eb1a91e15ea0bf
Codeyg12 <codey.gallup@gmail.com>
2023-01-27 22:40:17 -0700

    working on styling for current weather

--- src/CurrentWeather.jsx
+++ src/CurrentWeather.jsx
@@ -6,11 +6,11 @@ export default function CurrentWeather() {
       <div className="current">
         <div className="current-weather-card">
           <div className="left">
+            <div className="temp">{18}℉</div>
             <div className="city-info">
               <p className="name">Denver</p>
               <p className="description">Cloudy</p>
             </div>
-            <div className="temp">{18}℉</div>
           </div>
           <div className="right">
             <img
@@ -18,22 +18,30 @@ export default function CurrentWeather() {
               alt="weather-icon"
               className="weather-icon"
             />
-            <div className="feels-like">
-              <span className="label">Feels Like</span>
-              <span className="detail">{2}℉</span>
-            </div>
-            <div className="wind">
-              <span className="label">Wind Speed</span>
-              <span className="detail">{45}MPH</span>
-            </div>
-            <div className="humid">
-              <span className="label">Humidity</span>
-              <span className="detail">{77}%</span>
+            <div className="details">
+              <div className="feels-like">
+                <span className="label">Feels Like</span>
+                <span className="detail">{2}℉</span>
+              </div>
+              <div className="wind">
+                <span className="label">Wind Speed</span>
+                <span className="detail">{45}MPH</span>
+              </div>
+              <div className="humid">
+                <span className="label">Humidity</span>
+                <span className="detail">{77}%</span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <iframe style={{border: '1px solid rgba(0, 0, 0, 0.1)'}} width="800" height="450" src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FMycwaEKLYe5fFJUjN3aEVF%2FUntitled%3Fnode-id%3D0%253A1%26t%3DbBjAVLDBZgMTyIPy-1" allowfullscreen></iframe>
+      <iframe
+        style={{ border: "1px solid rgba(0, 0, 0, 0.1)" }}
+        width="800"
+        height="450"
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Ffile%2FMycwaEKLYe5fFJUjN3aEVF%2FUntitled%3Fnode-id%3D0%253A1%26t%3DbBjAVLDBZgMTyIPy-1"
+        allowfullscreen
+      ></iframe>
     </>
   );
 }
