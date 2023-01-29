2fb8125580e554b019a93be7bb0615ec4a995243
Codeyg12 <codey.gallup@gmail.com>
2023-01-28 20:22:32 -0700

    testing javascript for recieved data

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 let icon = '13d'
+let temper = 160.6
 
 weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+currentTemp.innerHTML = `${Math.round(temper)}<span class='fahrenheit'>℉</span>`
 
 function currentCondtions(city) {
     let api 
