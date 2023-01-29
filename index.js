bddf6e50352f16b4084a918bddad9aae0fa150eb
Codeyg12 <codey.gallup@gmail.com>
2023-01-28 19:06:58 -0700

    changing to vanilla js

---
+++ index.js
@@ -0,0 +1,7 @@
+let icon = '13d'
+
+weatherIcon.src = `http://openweathermap.org/img/wn/${icon}@2x.png`
+
+function currentCondtions(city) {
+    let api 
+}
--- src/api.js
+++
@@ -1,3 +0,0 @@
-// export const GOOGLE_API_KEY = 'AIzaSyAMuKSorRhJ3ShVUP8NWJzNCnovzvSEi9s'
-
-// export const OPEN_WEATHER_KEY = '0830fec5fefb765b207129fdb7fcdf86'
